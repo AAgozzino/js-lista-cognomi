40fce8d3eb4210e7614b75f5649150804f7a93c5
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-25 14:57:04 +0200

    Esercizio completo

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // Creo la lista di cognomi
-
+var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 // Chiedere all'utente il cognome
-
+var cognomeUtente = prompt("Inserisci il tuo cognome");
 // Inserire il cognome nella lista
-
+cognomi.push(cognomeUtente);
+console.log(cognomi);
 // Mettere gli elementi della lista in ordine alfabetico
-
+cognomi = cognomi.sort();
 // Stampare la lista in ordine alfabetico
-
+console.log(cognomi);
 // Stampare la posizione del nuovo cognome
+console.log(cognomi.indexOf(cognomeUtente) + 1);

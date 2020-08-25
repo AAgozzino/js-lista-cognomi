d57a3e1c5b72839c3bf58f2c08640eaf989d94ae
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-25 17:53:52 +0200

    Sort case insentive

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 // Creo la lista di cognomi
 var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
 // Chiedere all'utente il cognome
 var cognomeUtente = prompt("Inserisci il tuo cognome");
+
 // Inserire il cognome nella lista
 cognomi.push(cognomeUtente);
 console.log(cognomi);
+
 // Mettere gli elementi della lista in ordine alfabetico
-cognomi = cognomi.sort();
+//cognomi = cognomi.sort();
+var cognomiLower = [];
+for (var i = 0; i < cognomi.length; i++) {
+  cognomi[i].toLowerCase;
+  cognomiLower.push(cognomi[i]);
+  console.log(cognomi[i]);
+}
 // Stampare la lista in ordine alfabetico
-console.log(cognomi);
+console.log(cognomiLower);
+
 // Stampare la posizione del nuovo cognome
 console.log(cognomi.indexOf(cognomeUtente) + 1);

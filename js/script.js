3c80cf1f5613a4e1c71288d16ea66cd267765601
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-25 14:47:52 +0200

    Inizio progetto

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Creo la lista di cognomi
+
+// Chiedere all'utente il cognome
+
+// Inserire il cognome nella lista
+
+// Mettere gli elementi della lista in ordine alfabetico
+
+// Stampare la lista in ordine alfabetico
+
+// Stampare la posizione del nuovo cognome

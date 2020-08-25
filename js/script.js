e17b4fb58fa626360416ca4196a512492c2c9166
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-25 18:18:44 +0200

    Esercizio case sensitive

--- js/script.js
+++ js/script.js
@@ -5,19 +5,21 @@ var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 var cognomeUtente = prompt("Inserisci il tuo cognome");
 
 // Inserire il cognome nella lista
-cognomi.push(cognomeUtente);
+  if (cognomeUtente.charAt(0) == cognomeUtente.charAt(0).toLowerCase()) {
+    cognomeCapital = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
+    cognomi.push(cognomeCapital);
+  }
+  else {
+    cognomi.push(cognomeUtente);
+  };
+
 console.log(cognomi);
 
 // Mettere gli elementi della lista in ordine alfabetico
-//cognomi = cognomi.sort();
-var cognomiLower = [];
-for (var i = 0; i < cognomi.length; i++) {
-  cognomi[i].toLowerCase;
-  cognomiLower.push(cognomi[i]);
-  console.log(cognomi[i]);
-}
+cognomi = cognomi.sort();
+
 // Stampare la lista in ordine alfabetico
-console.log(cognomiLower);
+console.log(cognomi);
 
 // Stampare la posizione del nuovo cognome
 console.log(cognomi.indexOf(cognomeUtente) + 1);
